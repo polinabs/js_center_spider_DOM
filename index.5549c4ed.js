f360f8d17ca05fe7708a05f92612ad52e7efcb42
Polina <polinabondar4@gmail.com>
2024-08-06 19:41:47 +0300

    make build

---
+++ index.5549c4ed.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");Object.assign(e.style,{display:"flex",justifyContent:"center",alignItems:"center"}),t.style.position="relative";
+//# sourceMappingURL=index.5549c4ed.js.map
